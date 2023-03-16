43c6b575f704183fb4b164eb5922b7b72919951f
Ana Judith <ana-judth@hotmail.com>
2023-03-16 12:03:32 -0300

    Criando projeto vite, utilizando react e typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Form from './components/Form'
+import Lista from './components/Lista'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Form/>
+        <Lista />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+
+const Button = () => {
+    return (
+        <button>
+            Botão
+        </button>
+    )
+}
+
+export default Button;
